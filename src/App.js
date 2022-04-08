75fa5046626ab8f239015abacabbda721657326c
saurav darji <sauravdarji8793@gmail.com>
2022-04-09 01:14:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import { Routes, Route } from "react-router-dom";
 import Listing from "./Components/ProductListing";
-import { useCart } from "./cart-context";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import Footer from "./Components/Footer";
 
 
 export default function App() {
-  let navigate = useNavigate();
-  const { state } = useCart();
-  console.log(state)
-  useEffect(() => {
-    if (state.user.username) {
-      navigate("/");
-    } else {
-      navigate("signin");
-    }
-  }, [navigate, state.user.username]);
+
   return (
     <div className="App">
       <Navbar />
